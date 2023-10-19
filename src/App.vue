968e649c250b5be329231b355ec373cd6881750d
marvin <marvin010528@foxmail.com>
2023-10-19 11:09:15 +0800

    readme

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Book from '@/views/book/index.vue'
+import TestPage from '@/views/test-page/index.vue'
 </script>
 
 <template>
   <div class="p-10">
+    <TestPage></TestPage>
     <Book/>
   </div>
 </template>
---
+++ src/views/test-page/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{ user.name }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let user = ref<{id: number, name: string}>({
+    id: 1,
+    name: 'J.M'
+})
+
+const otherUser = {
+    id: 2,
+    name: 'M.R'
+}
+
+setTimeout(() => {
+    user.value = otherUser
+}, 1000)
+</script>
