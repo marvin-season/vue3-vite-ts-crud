66e51567e4a2fe498276e32b0692dc581bd846a9
marvin <marvin010528@foxmail.com>
2023-10-20 14:09:06 +0800

    异常处理

--- src/views/map/data.ts
+++ src/views/map/data.ts
@@ -30,25 +30,34 @@ export const getMapData: (
   currentLevelInfo: Ref<LevelInfoProps>,
   action?: MapAction
 ) => any = async (currentLevelInfo, action = "down") => {
+  let tempName = undefined;
   if (action == "up") {
     currentLevelInfo.value.level -= 1;
-    currentLevelInfo.value.nameStack.pop();
+    tempName = currentLevelInfo.value.nameStack.pop();
   }
 
   const { nameStack, level } = currentLevelInfo.value;
   const url = base
     .concat(geoLevelUrlMap[level])
     .concat("/")
-    .concat(nameStack.slice(-1)[0]);
+    .concat(nameStack.slice(-1)[0])
+    .concat(".json");
 
   console.log("url", url);
 
   try {
-    const response = await fetch(url.concat(".json"));
+    const response = await fetch(url);
     return await response.json();
   } catch (error) {
-    currentLevelInfo.value.level -= 1;
-    currentLevelInfo.value.nameStack.pop();
+    // rollback
+    if (action == "up" && tempName) {
+      currentLevelInfo.value.level += 1;
+      currentLevelInfo.value.nameStack.push(tempName);
+    } else {
+      currentLevelInfo.value.level -= 1;
+      currentLevelInfo.value.nameStack.pop();
+    }
+
     ElMessage.error("加载地图数据失败");
     throw new Error("加载地图数据失败");
   }
